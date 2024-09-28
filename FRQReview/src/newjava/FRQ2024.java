9de0922200e29056a1176d7f0a99fc50bba7f43c
s-jihe <s-jihe@lwsd.org>
2024-09-27 18:36:17 -0700

    test

---
+++ FRQReview/src/newjava/FRQ2024.java
@@ -0,0 +1,21 @@
+package newjava;
+public class FRQ2024 {
+public static void main(String[] args) {
+	top();
+	mid();
+	mid();
+	top();
+}
+public static void top() {
+System.out.println("   /*   ");
+System.out.println("  //**");
+System.out.println(" ///***");
+System.out.println("////****");
+}
+public static void mid() {
+System.out.println("****////");
+System.out.println(" ***/// ");
+System.out.println("  **//  ");
+System.out.println("   */   ");
+}
+}
